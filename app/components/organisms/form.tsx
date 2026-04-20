0bfd1aee333eddcd905f1c75e9137b05d9c4e638
Vishal R S <vishalrs@Vishals-MacBook-Air.local>
2026-04-20 23:10:31 +0530

    form submission migration

--- app/components/organisms/form.tsx
+++ app/components/organisms/form.tsx
@@ -5,6 +5,7 @@ const Form = ({
   initialState,
   reducerAction,
   children,
+  formProps,
 }: {
   initialState: Record<string, unknown>;
   reducerAction: (
@@ -20,13 +21,14 @@ const Form = ({
           formData: { type: string; payload: string } | FormData,
         ) => void;
       }) => React.ReactNode);
+  formProps?: React.FormHTMLAttributes<HTMLFormElement> & Record<string, unknown>;
 }) => {
   const [state, formAction, isPending] = useActionState(
     reducerAction,
     initialState,
   );
   return (
-    <form action={formAction} className="flex flex-col gap-4">
+    <form action={formAction} className="flex flex-col gap-4" {...formProps}>
       {typeof children === "function"
         ? children({ state, isPending, formAction })
         : children}{" "}
